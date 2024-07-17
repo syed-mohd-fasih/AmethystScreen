9764d7f6ffa28498e46335048504d8569f54899b
Syed Fasih <elite9145build@gmail.com>
2024-07-17 12:27:46 +0300

    Add project files.

---
+++ AmethystScreen/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AmethystScreen.Models
+{
+    public class Movie
+    {
+        // For Db and page redirects
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = new List<string>();
+        public string Description { get; set; } = string.Empty;
+        [JsonPropertyName("img")]
+        public string Image { get; set; } = string.Empty;
+        public double Rating { get; set; }
+        public string VideoUrl { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+    }
+}
