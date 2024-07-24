2026ca2e1d05266fc19aff73a500fb052c14598f
Syed Fasih <elite9145build@gmail.com>
2024-07-24 16:35:56 +0300

    Added User Identity class for Account creation, authorization and authentication

--- AmethystScreen/Program.cs
+++ AmethystScreen/Program.cs
@@ -1,6 +1,8 @@
 using AmethystScreen.Data;
 using AmethystScreen.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using AmethystScreen.Areas.Identity.Data;
 
 namespace AmethystScreen
 {
@@ -14,6 +16,9 @@ namespace AmethystScreen
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlite(builder.Configuration.GetConnectionString("AmethystMovieContext")));
+            builder.Services.AddDbContext<UserDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("AmethystUserContext")));
+            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<UserDbContext>();
             builder.Services.AddScoped<MoviesDirectoryService>();
 
             builder.Logging.ClearProviders();
@@ -30,13 +35,22 @@ namespace AmethystScreen
                 app.UseHsts();
             }
 
+            app.Use(async (context, next) =>
+            {
+                var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+                logger.LogInformation("Request URL: {URL}", context.Request.Path);
+                await next.Invoke();
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}"
